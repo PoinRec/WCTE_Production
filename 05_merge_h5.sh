89eee87f627b854cd29e0d27ae8c8d9f2b4257e2
Zhihao Wang <poinrec@gmail.com>
2025-08-16 22:16:05 +0900

    Adjusted formatting in merge script

--- 05_merge_h5.sh
+++ 05_merge_h5.sh
@@ -3,4 +3,9 @@
 set -euo pipefail
 
 singularity exec -B ~:/mnt ~/Images/larcv2_ub2204-cuda121-torch251-larndsim-2025-03-20.sif \
-        bash -lc 'python /mnt/Simulations/DataTools/root_utils/merge_h5.py /mnt/Data/MC_e/wcsim_wCDS_e-_Uniform_0_1200MeV_dighit.h5 /mnt/Data/MC_gamma/wcsim_wCDS_gamma_Uniform_0_1200MeV_dighit.h5 -o /mnt/Data/wcsim_wCDS_e-_gamma_Uniform_0_1200MeV_dighit.h5'
+	bash -lc '
+		python /mnt/Simulations/DataTools/root_utils/merge_h5.py \
+      		/mnt/Data/MC_e/wcsim_wCDS_e-_Uniform_0_1200MeV_dighit.h5 \
+      		/mnt/Data/MC_gamma/wcsim_wCDS_gamma_Uniform_0_1200MeV_dighit.h5 \
+      		-o /mnt/Data/wcsim_wCDS_e-_gamma_Uniform_0_1200MeV_dighit.h5
+  		'
